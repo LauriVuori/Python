a8b6fcb30ba2a5656ea7a1b97365454f0da48fdb
Lauri Vuori <55624232+LauriVuori@users.noreply.github.com>
2020-08-31 21:29:46 +0300

    Drawing rectangle on video

--- Projects/Rs/Main.py
+++ Projects/Rs/Main.py
@@ -1,17 +1,31 @@
 import cv2
 import numpy as np
+import MouseClicks as MC
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
 
 class Buttons(App):
     def build(self):
-        button = Button(text="Function 1", pos=(25,400), size_hint = (.25, .18))
-        button.bind(on_press=self.press)
-        return button
-    def press(self,instance):
-        print("Pressed")
+        layoutsize = ((300,300))
+        layout = FloatLayout(size=(layoutsize))
 
+        button1 = Button(text="Function 1", size_hint=(.1, .1),
+                         pos_hint={'x':.1, 'y':.9})
 
+        
+        button2 = Button(text="Function 2", size_hint=(.1, .1),
+                         pos_hint={'x':.1, 'y':.8})
+        
+        button1.bind(on_press=self.press1)
+        button2.bind(on_press=self.press2)
+        layout.add_widget(button1)
+        layout.add_widget(button2)
+        return layout
+    def press1(self,instance):
+        MC.test()
+    def press2(self,instance):
+        print("kakkosfunc")
 
 
 def main():
--- Projects/Rs/MouseClicks.py
+++ Projects/Rs/MouseClicks.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from PIL import ImageGrab
 #     events
 #     EVENT_MOUSEMOVE     = 0,
 #     EVENT_LBUTTONDOWN   = 1,
@@ -14,13 +15,47 @@ import numpy as np
 #     EVENT_MOUSEWHEEL    = 10,
 #     EVENT_MOUSEHWHEEL   = 11,
 
-
 drawing=False # true if mouse is pressed
 mode=True # if True, draw rectangle. Press 'm' to toggle to curve
+# img = np.zeros((512,512,3), np.uint8)
+
+# WhiteImg = np.zeros([512,512,1],dtype=np.uint8)
+# WhiteImg.fill(255)
+
+# img = WhiteImg
+xList = []
+yList = []
+
+def test():    
+    global img, xList, yList 
+    cv2.namedWindow('image')
+    STOP = 0
+    while(STOP == 0):
+        img = ImageGrab.grab(bbox=(0,0,1980,1240))
+        img = np.uint8(img)
+
+        
+        # every move calls function draw_circle, 
+        cv2.setMouseCallback('image', rectangle_drawing)
+        if len(xList) == 2:
+            img = cv2.rectangle(img, (xList[0],yList[0]), (xList[1],xList[1]), (0,0,255))
+            xList = []
+            yList = []
+
+
+        cv2.imshow('image',img)
+
+        k = cv2.waitKey(1)
+        if k == 27:
+            cv2.destroyWindow('image')
+            STOP = 1
+        elif k == ord('a'):
+            print(mouseX, mouseY)
+
 
 # mouse callback function
-def interactive_drawing(event,x,y,flags,param):
-    global ix,iy,drawing, mode
+def interactive_drawing(event, x, y,flags, param):
+    global ix, iy, drawing, mode, img, xList, yList
 
     if event==cv2.EVENT_LBUTTONDOWN:
         drawing=True
@@ -29,28 +64,43 @@ def interactive_drawing(event,x,y,flags,param):
     elif event==cv2.EVENT_MOUSEMOVE:
         if drawing==True:
             if mode==True:
-                cv2.line(img,(ix,iy),(x,y),(0,0,255),10)
+                img = cv2.line(img,(ix,iy),(x,y),(0,0,255),10)
                 ix=x
                 iy=y
+                xList.append(x)
+                yList.append(y)
+
     elif event==cv2.EVENT_LBUTTONUP:
         drawing=False
         if mode==True:
-            cv2.line(img,(ix,iy),(x,y),(0,0,255),10)
-    return x,y
+            img = cv2.line(img,(ix,iy),(x,y),(0,0,255),10)
+            xList.append(x)
+            yList.append(y)
+
+def rectangle_drawing(event, x, y,flags, param):
+    global ix, iy, drawing, mode, img, xList, yList
+
+    if event==cv2.EVENT_LBUTTONDOWN:
+        drawing=True
+        ix,iy=x,y
+        xList.append(ix)
+        yList.append(iy)
 
 
-img = np.zeros((512,512,3), np.uint8)
-cv2.namedWindow('image')
+    elif event==cv2.EVENT_MOUSEMOVE:
+        if drawing==True:
+            if mode==True:
+                img = cv2.line(img,(ix,iy),(x,y),(0,0,255),10)
+                ix=x
+                iy=y
 
-# every move calls function draw_circle, 
-cv2.setMouseCallback('image', interactive_drawing)
+    elif event==cv2.EVENT_LBUTTONUP:
+        drawing=False
+        if mode==True:
+            img = cv2.line(img,(ix,iy),(x,y),(0,0,255),10)
+            xList.append(x)
+            yList.append(y)
+def Drawlist(Dots):
+    pass
 
-
-
-while(1):
-    cv2.imshow('image',img)
-    k = cv2.waitKey(20) & 0xFF
-    if k == 27:
-        break
-    elif k == ord('a'):
-        print(mouseX,mouseY)
+test()
