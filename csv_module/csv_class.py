8ac9d6e178a0531f6b3d9c42f6c187ab731ef574
LauriVuori <Laurivuori.lv@gmail.com>
2021-09-01 11:41:49 +0300

    module to work with csv

---
+++ csv_module/csv_class.py
@@ -0,0 +1,32 @@
+import csv
+
+
+class csv_class:
+    """
+    Class description:
+    -----------------
+    this is class
+
+    Brief:
+    -----------------
+    This class does
+
+    Attributes:
+    -----------------
+    this_is_list : list
+
+    Methods:
+    -----------------
+    read
+    write
+    create
+    find colum
+    print csv user to see whats in it
+    get data from colum
+    """
+    def __init__(self):
+        pass
+
+
+
+print("test")
