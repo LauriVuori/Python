35116aa3545efbc4f487980e3f57672cd20192f9
LauriVuori <Laurivuori.lv@gmail.com>
2021-09-03 11:12:18 +0300

    update

--- csv_module/csv_class.py
+++ csv_module/csv_class.py
@@ -24,9 +24,27 @@ class csv_class:
     print csv user to see whats in it
     get data from colum
     """
-    def __init__(self):
-        pass
+    def __init__(self, filename="example.csv", delimeter=";"):
+        self.filename = filename
+        self.delimeter = delimeter
 
+    def set_filename(self, filename):
+        self.filename = filename
 
+    def get_filename(self):
+        print("Current file name is: " + self.filename)
+    
+    def create_csv(self):
+        new_file = open(self.filename, "wb")
+        print("Created file: " + new_file.name)
+        new_file.close()
+        # with open(self.filename, "wb") as csvfile:
+            
+# with open('eggs.csv', newline='') as csvfile:
+# ...     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+# ...     for row in spamreader:
+# ...         print(', '.join(row))
 
-print("test")
+test = csv_class()
+test.create_csv()
+
